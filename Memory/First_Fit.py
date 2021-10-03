9b9dc6e9bd721beadef5c900e3aef523bd0aec8b
Naitik Yadav <66974007+n4itik@users.noreply.github.com>
2021-10-03 19:28:31 +0530

    Add files via upload

---
+++ Memory/First_Fit.py
@@ -0,0 +1,27 @@
+def first_fit(block_sizes, process_sizes, m, n):
+    allocation = [-1] * n
+
+    for i in range(n):
+        for j in range(m):
+            if block_sizes[j] >= process_sizes[i]:
+                allocation[i] = j
+                block_sizes[j] -= process_sizes[i]
+                break
+
+    print("Process Size\tBlock No.")
+    for i in range(n):
+        print(process_sizes[i], end="\t\t")
+
+        if allocation[i] != -1:
+            print(allocation[i] + 1)
+        else:
+            print("NA")
+
+
+m = int(input("Enter no. of blocks: "))
+n = int(input("Enter no. of processes: "))
+
+block_sizes = [int(i) for i in input("Enter block sizes: ").split()]
+process_sizes = [int(i) for i in input("Enter process sizes: ").split()]
+
+first_fit(block_sizes, process_sizes, m, n)
